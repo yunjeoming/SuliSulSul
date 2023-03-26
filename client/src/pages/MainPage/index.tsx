7339c3f33703a2646e6c14547b1f03ab2fc19416
yunjeoming <yunjeoming@gmail.com>
2023-03-26 14:31:19 +0900

    feat: add tailwind css

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainPage = () => {
   // 홈페이지 or 메인페이지
-  return <div>MainPage</div>;
+  return <div className='text-blue-600/100'>MainPage</div>;
 };
 
 export default MainPage;
