7dcc1e9176ef963f6109b2987f1aaf3f52631150
yunjeoming <yunjeoming@gmail.com>
2023-04-18 23:45:44 +0900

    add: userNm, pwd

--- client/src/pages/AlcoholDetailPage/AddReview.tsx
+++ client/src/pages/AlcoholDetailPage/AddReview.tsx
@@ -19,16 +19,45 @@ const AddReview = ({ alcohol, onClose, getReviews }: Props) => {
   const gradeRef = useRef<HTMLSpanElement | null>(null);
   const titleRef = useRef<HTMLInputElement | null>(null);
   const contentRef = useRef<HTMLTextAreaElement | null>(null);
+  const userNmRef = useRef<HTMLInputElement | null>(null);
+  const reviewPwdRef = useRef<HTMLInputElement | null>(null);
 
   const navigate = useNavigate();
 
   const clickSaveBtn = () => {
     if (!gradeRef.current?.textContent) {
       alert('별점을 선택해주세요!!');
+      gradeRef.current?.focus();
       return;
     }
+
     if (!titleRef.current?.value) {
       alert('제목을 입력해주세요!!');
+      titleRef.current?.focus();
+      return;
+    }
+
+    if (!contentRef.current?.value) {
+      alert('내용을 입력해주세요!!');
+      contentRef.current?.focus();
+      return;
+    }
+
+    if (!userNmRef.current?.value) {
+      alert('작성자를 입력해주세요!!');
+      userNmRef.current?.focus();
+      return;
+    }
+
+    if (userNmRef.current?.value.length > 10) {
+      alert('작성자는 최대 10자까지 입력해주세요!!');
+      userNmRef.current?.focus();
+      return;
+    }
+
+    if (!reviewPwdRef.current?.value) {
+      alert('비밀번호를 입력해주세요!!');
+      reviewPwdRef.current?.focus();
       return;
     }
 
@@ -57,15 +86,21 @@ const AddReview = ({ alcohol, onClose, getReviews }: Props) => {
     const grade = gradeRef.current?.textContent?.split('점')[0];
     const title = titleRef.current?.value;
     const content = contentRef.current?.value;
+    const userNm = userNmRef.current?.value;
+    const reviewPwd = reviewPwdRef.current?.value;
     console.log('grade', grade);
     console.log('title', title);
     console.log('content', content);
+    console.log('userNm', userNm);
+    console.log('reviewPwd', reviewPwd);
 
     axios
       .post(`/`, {
         grade,
         title,
         content,
+        userNm,
+        reviewPwd 
       })
       .then((res) => {
         // status code 200일때 redirect. 실패하면 alert 다시 띄우기
@@ -91,8 +126,8 @@ const AddReview = ({ alcohol, onClose, getReviews }: Props) => {
         <AlcoholListItem alcohol={alcohol} showingType="listType" isNotLink />
       </div>
       <div className="flex-grow">
-        <section className="flex flex-col items-center py-4 [&>*:not(:last-child)]:mb-2">
-          <div className="mb-2">몇 점을 주시겠어요?</div>
+        <section className="flex flex-col items-center p-4 [&>*:not(:last-child)]:mb-2">
+          <div>몇 점을 주시겠어요?</div>
           <div className="text-3xl text-gray-300 relative" onClick={clickStar}>
             <div className={`text-yellow-300 absolute top-0 left-0 w-0 overflow-hidden cursor-pointer`} ref={fillRef}>
               <span>★★★★★</span>
@@ -103,13 +138,16 @@ const AddReview = ({ alcohol, onClose, getReviews }: Props) => {
           </div>
           <span className="text-sm" ref={gradeRef}></span>
         </section>
-        <section className="flex flex-col items-center p-4 [&>*:not(:last-child)]:mb-4">
-          <div>리뷰를 남겨주세요.</div>
-          <input className="w-full p-2" placeholder="제목은 필수 사항입니다." ref={titleRef} />
-          <textarea className="w-full p-2" placeholder="내용은 선택 사항입니다." rows={5} ref={contentRef} />
-          <div className="">
-            {/* <label>제목</label> */}
-            {/* <label>내용</label> */}
+        <section className="flex flex-col items-center p-4 [&>*:not(:last-child)]:mb-2">
+          <div>리뷰를 남겨주세요</div>
+          <input className="w-full p-2" placeholder="제목" ref={titleRef} />
+          <textarea className="w-full p-2" placeholder="내용" rows={5} ref={contentRef} />
+        </section>
+        <section className="flex flex-col items-center p-4 [&>*:not(:last-child)]:mb-2">
+          <div>작성자 정보를 알려주세요</div>
+          <div className='flex items-center justify-between'>
+            <input className="w-[58%] p-2" placeholder="작성자" ref={userNmRef} />
+            <input className="w-2/5 p-2" placeholder="비밀번호" ref={reviewPwdRef} type="password" />
           </div>
         </section>
       </div>
