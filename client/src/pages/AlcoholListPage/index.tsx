0189be9589d2f617897e127e81f5eb827067afc4
yunjeoming <yunjeoming@gmail.com>
2023-04-26 22:34:28 +0900

    fix: add padding of icons

--- client/src/pages/AlcoholListPage/index.tsx
+++ client/src/pages/AlcoholListPage/index.tsx
@@ -35,12 +35,12 @@ const AlcoholListPage = () => {
       headerChildComponent={
         <>
           <IconButton
-            styles={`border-l border-r border-stone-950 hover:bg-gray-100`}
+            styles={`border-l border-r border-stone-950 p-3 hover:bg-gray-100`}
             onClick={() => handleClickShowingType('listType')}
           >
             <BsListUl color={`${showingType === 'listType' ? '#000' : '#adadad'}`} />
           </IconButton>
-          <IconButton styles={`hover:bg-gray-100`} onClick={() => handleClickShowingType('gridType')}>
+          <IconButton styles={`p-3 hover:bg-gray-100`} onClick={() => handleClickShowingType('gridType')}>
             <RxGrid color={`${showingType === 'gridType' ? '#000' : '#adadad'}`} />
           </IconButton>
         </>
