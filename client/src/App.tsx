3c042afc9bc6e819f96cbe9239dbfe85158400b8
yunjeoming <yunjeoming@gmail.com>
2023-08-03 23:21:53 +0900

    fix: use lazy component instead of esmodule import
    fix: use suspense

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AdminMainPage from './pages/Admin/MainPage';
-import AlcoholDetailPage from './pages/AlcoholDetailPage';
-import AlcoholListPage from './pages/AlcoholListPage';
-import NotFoundPage from './pages/NotFoundPage';
 import { Styles } from './constants/Styles';
-import SearchListPage from './pages/SearchListPage';
-import ReviewListPage from './pages/ReviewListPage';
-import MainPage from './pages/MainPage';
 import { getClient } from './queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
+
+const MainPage = lazy(() => import('./pages/MainPage'));
+const AlcoholListPage = lazy(() => import('./pages/AlcoholListPage'));
+const AlcoholDetailPage = lazy(() => import('./pages/AlcoholDetailPage'));
+const SearchListPage = lazy(() => import('./pages/SearchListPage'));
+const ReviewListPage = lazy(() => import('./pages/ReviewListPage'));
+const AdminMainPage = lazy(() => import('./pages/Admin/MainPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
 function App() {
   const queryClient = getClient();
@@ -17,16 +18,18 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <div className="h-screen bg-gray-200 flex justify-center items-stretch">
         <div className={`relative ${Styles.MAIN_BACKGROUND_COLOR} w-96 overflow-auto`}>
-          <Routes>
-            <Route path="/" element={<MainPage />}>
-              <Route path="c/:no" element={<AlcoholListPage />} />
-              <Route path="alcs/:no" element={<AlcoholDetailPage />} />
-              <Route path="search" element={<SearchListPage />} />
-              <Route path="reviews/:no" element={<ReviewListPage />} />
-            </Route>
-            <Route path="/admin/*" element={<AdminMainPage />} />
-            <Route path="/*" element={<NotFoundPage />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<MainPage />}>
+                <Route path="c/:no" element={<AlcoholListPage />} />
+                <Route path="alcs/:no" element={<AlcoholDetailPage />} />
+                <Route path="search" element={<SearchListPage />} />
+                <Route path="reviews/:no" element={<ReviewListPage />} />
+              </Route>
+              <Route path="/admin/*" element={<AdminMainPage />} />
+              <Route path="/*" element={<NotFoundPage />} />
+            </Routes>
+          </Suspense>
         </div>
       </div>
     </QueryClientProvider>
--- client/src/pages/Admin/MainPage/index.tsx
+++ client/src/pages/Admin/MainPage/index.tsx
@@ -1,11 +1,13 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AdminAlcoholListPage from '../AlcoholListPage';
-import AdminAlcoholDetailPage from '../AlcoholDetailPage';
 import AddAlcohol from './AddAlcohol';
 import AdminHeader from '../../../components/Header/AdminHeader';
-import AdminSearchListPage from '../SearchListPage';
 import useAddPage from '../../../hooks/useAddPage';
 import MainLayout from '../../../layout/MainLayout';
+
+const AdminAlcoholListPage = lazy(() => import('../AlcoholListPage'));
+const AdminAlcoholDetailPage = lazy(() => import('../AlcoholDetailPage'));
+const AdminSearchListPage = lazy(() => import('../SearchListPage'));
 
 const AdminMainPage = () => {
   const { isOpenAddPage, openAddPage, closeAddPage } = useAddPage();
@@ -14,12 +16,14 @@ const AdminMainPage = () => {
     <>
       <AdminHeader handleClickNewAlcohol={openAddPage} />
       <MainLayout>
-        <Routes>
-          <Route path="" element={<AdminAlcoholListPage />} />
-          <Route path="login" element={<AdminAlcoholListPage />} />
-          <Route path="alcs/:no" element={<AdminAlcoholDetailPage />} />
-          <Route path="search" element={<AdminSearchListPage />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="" element={<AdminAlcoholListPage />} />
+            <Route path="login" element={<AdminAlcoholListPage />} />
+            <Route path="alcs/:no" element={<AdminAlcoholDetailPage />} />
+            <Route path="search" element={<AdminSearchListPage />} />
+          </Routes>
+        </Suspense>
       </MainLayout>
       {isOpenAddPage && <AddAlcohol onClose={closeAddPage} />}
     </>
