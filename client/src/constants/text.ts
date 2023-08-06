46d40d3fdcafe2842d818141459d2de299738ba4
yunjeoming <yunjeoming@gmail.com>
2023-08-06 20:35:11 +0900

    fix: change all to 전체 and add no alcohol text

--- client/src/constants/text.ts
+++ client/src/constants/text.ts
@@ -8,4 +8,5 @@ export const TextConstants = {
   FAIL_DELETE: '삭제에 실패했어요 \n 잠시 후 다시 시도해주세요',
   FAIL_ADD: '등록에 실패했어요 \n\n ⚠️ 이모티콘은 등록이 되지 않아요 ⚠️',
   CAUTION: '⚠️ 내용이 저장되지 않았어요 ⚠️ \n나가시겠어요?',
+  NO_ALCOHOL: '등록된 술이 없어요'
 };
